a0d04ed1eb27f611f82ab80a23689e64e1c13a74
hasanmbstu13 <hasanmbstu13@gmail.com>
2014-07-24 08:28:13 +0600

    Simple Interest Calculator.

--- BankInterestCalculatorProj/index.php
+++ BankInterestCalculatorProj/index.php
@@ -15,18 +15,21 @@ and open the template in the editor.
             Principle Amount:<input type="text" name="principleAmountText"/><br />
             Annual Interest Rate:<input type="text" name="InterestRateText"/> %<br />
             Time Period:<input type="text" name="yearText"/> year<br />
-            <input type="submit" name="calculateTotalButton" value ="Calculate"/>
+            <input type="submit" name="calculateButton" value ="Calculate"/>
         </form>
         <?php
         require_once 'interestcalculator.php';
-        if(isset($_GET['calculateTotalButton']))
+        if(isset($_GET['calculateButton']))
         {
-            $interest_calculator = new InterestCalcultor();
+            $interest_calculator = new InterestCalculator();
             $interest_calculator->principle_amount = $_GET['principleAmountText'];
             $interest_calculator->intrest_rate_of_a_year = $_GET['InterestRateText'];
             $interest_calculator->time_period = $_GET['yearText'];
+            $total_amount = $interest_calculator->get_total_amount();
+            echo $total_amount;
             
         }
+        
         ?>
     </body>
 </html>
---
+++ BankInterestCalculatorProj/interestcalculator.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of calcultor
+ *
+ * @author Mainul Hasan
+ */
+class InterestCalculator {
+    public $principle_amount;
+    public $intrest_rate_of_a_year;
+    public $time_period;
+    
+    public function get_total_amount()
+    {
+        $interest_amount_in_a_year = $this->principle_amount*($this->intrest_rate_of_a_year/100);
+        $interest_amount_in_given_time_period = $interest_amount_in_a_year*$this->time_period;
+        $total_amount = $this->principle_amount + $interest_amount_in_given_time_period;
+        return $total_amount;
+    }
+}
