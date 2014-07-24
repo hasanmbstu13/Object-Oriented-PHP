01388a43498686904fdec2e388a0ee402c522fd9
hasanmbstu13 <hasanmbstu13@gmail.com>
2014-07-24 14:26:36 +0600

    Make a Lunch Bill Calculator.

--- BankInterestCalculatorProj/simpleinterestcalculator.php
+++ BankInterestCalculatorProj/simpleinterestcalculator.php
@@ -11,15 +11,15 @@
  *
  * @author Mainul Hasan
  */
-class InterestCalculator {
+class SimpleInterestCalculator {
     public $principle_amount;
     public $intrest_rate_of_a_year;
     public $time_period;
     
     public function get_total_amount()
     {
-        $interest_amount_in_a_year = $this->principle_amount*($this->intrest_rate_of_a_year/100);
-        $interest_amount_in_given_time_period = $interest_amount_in_a_year*$this->time_period;
+        $interest_amount_in_a_year = $this->principle_amount * ($this->intrest_rate_of_a_year / 100);
+        $interest_amount_in_given_time_period = $interest_amount_in_a_year * $this->time_period;
         $total_amount = $this->principle_amount + $interest_amount_in_given_time_period;
         return $total_amount;
     }
