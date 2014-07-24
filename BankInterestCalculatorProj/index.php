e66b9c0180df2ebd50ef2284c5ad89dc7d8c2d13
hasanmbstu13 <hasanmbstu13@gmail.com>
2014-07-24 08:09:25 +0600

    A project which get the employee information and display it in output.

---
+++ BankInterestCalculatorProj/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Simple Interest Calculator</h1>
+        <form action="index.php" method="get">
+            Principle Amount:<input type="text" name="principleAmountText"/><br />
+            Annual Interest Rate:<input type="text" name="InterestRateText"/> %<br />
+            Time Period:<input type="text" name="yearText"/> year<br />
+            <input type="submit" name="calculateTotalButton" value ="Calculate"/>
+        </form>
+        <?php
+        require_once 'interestcalculator.php';
+        if(isset($_GET['calculateTotalButton']))
+        {
+            $interest_calculator = new InterestCalcultor();
+            $interest_calculator->principle_amount = $_GET['principleAmountText'];
+            $interest_calculator->intrest_rate_of_a_year = $_GET['InterestRateText'];
+            $interest_calculator->time_period = $_GET['yearText'];
+            
+        }
+        ?>
+    </body>
+</html>
---
+++ BankInterestCalculatorProj/interestcalcultor.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of calcultor
+ *
+ * @author Mainul Hasan
+ */
+class InterestCalcultor {
+    public $principle_amount;
+    public $intrest_rate_of_a_year;
+    public $time_period;
+    
+}
