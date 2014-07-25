d2cdef8d91dbed299b91ab86e2c7b3b14cde1149
hasanmbstu13 <hasanmbstu13@gmail.com>
2014-07-25 09:40:48 +0600

    A program will print the person full name and reverse name. --Input is hard coded value.

---
+++ CalculatorWebApp/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <form>
+       First No:<input type="text" name="firstNumberText"/><br/>
+       Second No: <input type="text" name="secondNumberText"/><br/>
+       <input type="submit" name="addButton" value="Add"/>
+       <input type="submit" name="subtractButton" value="Subtract"/>
+       <input type="submit" name="multiplyButton" value="Multiply"/>
+       <input type="submit" name="divideButton" value="Divide"/>
+    </form>
+    <body>
+        <?php
+        if(isset($_GET['addButton']))
+        {
+            
+        }
+        ?>
+    </body>
+</html>
---
+++ PersonProj/person.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Description of person
+ *
+ * @author Mainul Hasan
+ */
+class Person {
+    public $first_name;
+    public $middle_name;
+    public $last_name;
+    
+    public function get_full_name()
+    {
+        return $this->first_name.' '.$this->middle_name.' '.$this->last_name;
+    }
+    
+    public function get_reverse_name()
+    {
+        $full_name = $this->get_full_name();
+        $reverse_name = strrev($full_name);
+        return $reverse_name;     
+    }
+    
+    
+}
