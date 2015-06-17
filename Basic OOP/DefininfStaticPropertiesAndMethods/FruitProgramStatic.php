4e65174ce499e83b834c5c7f6cbaebce92b70117
manwarcuet <manwar.cuet@gmail.com>
2015-06-17 12:52:31 +0600

    Implementing interace.

--- Basic OOP/DefininfStaticPropertiesAndMethods/FruitProgramStatic.php
+++ Basic OOP/DefininfStaticPropertiesAndMethods/FruitProgramStatic.php
@@ -5,6 +5,7 @@ $myApples = 1;
 $myOranges = 2;
 $myBananas = 3;
 
-$rtnVal = FruitStatic::addFruit($myApples, $myOranges, $myBananas);
+$rtnVal = FruitStatic::addFruit($myApples, $myOranges, $myBananas); //for static method and variable we never need to instantiate a class
+																	//we directly access the variable and method using :: or scope resolution operator
 
 print "You have $rtnVal pieces of fruit";
